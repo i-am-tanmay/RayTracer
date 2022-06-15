d19f48be8adba8ebf934a96b63a0a4f782eeb44c
i-am-tanmay <iamtanmay77@gmail.com>
2022-06-15 14:40:46 -0400

    render image in imgui

--- source/Raytracer.Main/Program.cpp
+++ source/Raytracer.Main/Program.cpp
@@ -35,13 +35,19 @@ static ID3D11RenderTargetView* g_mainRenderTargetView{ nullptr };
 // dx helper functions
 bool CreateDeviceD3D(HWND hWnd, void* img_buffer, ID3D11ShaderResourceView*& img_buffer_srv);
 void CleanupDeviceD3D(ID3D11ShaderResourceView*& img_buffer_srv);
+bool CreateRenderTarget();
+void CleanupRenderTarget();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
 int main(int, char**)
 {
 	std::uint8_t* img_buffer = new std::uint8_t[img_width * img_height * 4];
-	std::memset(img_buffer, 255, img_width * img_height * 4);
+	ImVec2 windowsize{ img_width, img_height };
+
+	// RAYTRACING PROPERTIES
+	const std::size_t samples_per_pixel = 32;
+	const std::size_t bounce_limit = 8;
 
 #pragma region INIT DX11 & IMGUI
 
@@ -83,10 +89,6 @@ int main(int, char**)
 #pragma endregion
 
 #pragma region RayTracing
-
-	// PROPERTIES
-	const std::size_t samples_per_pixel = 32;
-	const std::size_t bounce_limit = 8;
 
 	// WORLD
 	RenderObjectList world;
@@ -142,57 +144,10 @@ int main(int, char**)
 
 	ThreadPool threadpool{ std::max(std::thread::hardware_concurrency(),2u) - 1u };
 
-	for (std::size_t i = 0; i < img_height; ++i)
-	{
-		//std::cout << "\rScanlines remaining: " << img_height - i - 1 << ' ' << std::flush;
-		for (std::size_t ii = 0; ii < img_width; ++ii)
-		{
-			threadpool.EnqueueTask([&, i, ii]
-				{
-					color pixel_color{ 0,0,0 };
-
-					for (std::size_t sample = 0; sample < samples_per_pixel; ++sample)
-					{
-						precision u = (ii + get_random01()) / (img_width - 1);
-						precision v = (i + get_random01()) / (img_height - 1);
-						pixel_color += ray_color(camera.get_ray(u, v), world, bounce_limit);
-					}
-
-					write_color(&img_buffer[(i * img_width + ii) * 3], pixel_color, samples_per_pixel);
-				});
-		}
-	}
+	bool renderstarted = false;
+	g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
 
 #pragma endregion
-
-	//// texture description
-	//D3D11_TEXTURE2D_DESC tex_description;
-	//ZeroMemory(&tex_description, sizeof(tex_description));
-	//tex_description.Width = static_cast<UINT>(img_width);
-	//tex_description.Height = static_cast<UINT>(img_height);
-	//tex_description.MipLevels = 1;
-	//tex_description.ArraySize = 1;
-	//tex_description.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	//tex_description.SampleDesc.Count = 1;
-	//tex_description.Usage = D3D11_USAGE_DYNAMIC;
-	//tex_description.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-	//tex_description.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-
-	//// bind image buffer to texture sub-resource
-	//D3D11_SUBRESOURCE_DATA tex_resource;
-	//tex_resource.pSysMem = img_buffer;
-	//tex_resource.SysMemPitch = tex_description.Width * 4;
-	//tex_resource.SysMemSlicePitch = 0;
-
-	//// texture view description
-	//D3D11_SHADER_RESOURCE_VIEW_DESC srv_desc;
-	//ZeroMemory(&srv_desc, sizeof(srv_desc));
-	//srv_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	//srv_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	//srv_desc.Texture2D.MipLevels = 1;
-	//srv_desc.Texture2D.MostDetailedMip = 0;
-
-	
 
 	// Main loop
 	bool done = false;
@@ -220,24 +175,54 @@ int main(int, char**)
 
 		// render stuff START
 
-		// create the texture view
-		//ID3D11Texture2D* texture{ nullptr };
-		//ID3D11Resource* pBackBuffer{ nullptr };
-		//g_mainRenderTargetView->GetResource(&pBackBuffer);
-		//g_pd3dDevice->CreateTexture2D(&tex_description, &tex_resource, &texture);
-		//g_pd3dDeviceContext->CopyResource(pBackBuffer, texture);
-		//pBackBuffer->Release();
-		//texture->Release();
+		{
+			ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+			ImGui::SetNextWindowSize(windowsize, ImGuiCond_Always);
+			ImGui::Begin("output image", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoBringToFrontOnFocus);
+
+			ID3D11Resource* img_buffer_resource;
+			img_buffer_srv->GetResource(&img_buffer_resource);
+
+			D3D11_MAPPED_SUBRESOURCE mapped_resource;
+			g_pd3dDeviceContext->Map(img_buffer_resource, 0, D3D11_MAP_WRITE_DISCARD, 0, &mapped_resource);
+			memcpy(mapped_resource.pData, img_buffer, img_width * img_height * 4 * sizeof(uint8_t));
+			g_pd3dDeviceContext->Unmap(img_buffer_resource, 0);
+
+			ImGui::Image(img_buffer_srv, windowsize);
+
+			ImGui::End();
+		}
 
 		// render stuff END
 
 #pragma region imgui UIs
+
 		{
 			ImGui::Begin("thou scene.");
 
-			if (ImGui::Button("let us begin."))
+			if (ImGui::Button("let us begin.") && !renderstarted)
 			{
+				renderstarted = true;
 
+				for (std::size_t i = 0; i < img_height; ++i)
+				{
+					for (std::size_t ii = 0; ii < img_width; ++ii)
+					{
+						threadpool.EnqueueTask([&, i, ii]
+							{
+								color pixel_color{ 0,0,0 };
+
+								for (std::size_t sample = 0; sample < samples_per_pixel; ++sample)
+								{
+									precision u = (ii + get_random01()) / (img_width - 1);
+									precision v = ((img_height - i - 1) + get_random01()) / (img_height - 1);
+									pixel_color += ray_color(camera.get_ray(u, v), world, bounce_limit);
+								}
+
+								write_color(&img_buffer[(i * img_width + ii) * 4], pixel_color, samples_per_pixel);
+							});
+					}
+				}
 			}
 
 			ImGui::Text("%.3f ms / %.1f FPS", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -247,10 +232,9 @@ int main(int, char**)
 
 #pragma endregion
 
-		// render
+		// render imgui
 		ImGui::Render();
 		//const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
-		g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
 		//g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
 		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 
@@ -270,110 +254,6 @@ int main(int, char**)
 	::UnregisterClass(wc.lpszClassName, wc.hInstance);
 
 	return 0;
-
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-	//// OUTPUT IMAGE
-	//const std::size_t img_width = 1280;
-	//const std::size_t img_height = static_cast<int>(img_width / aspect_ratio);
-
-	//std::uint8_t* img_rgb = new std::uint8_t[img_width * img_height * 3];
-
-	//// PROPERTIES
-	//const std::size_t samples_per_pixel = 500;
-	//const std::size_t bounce_limit = 50;
-
-	//// WORLD
-	//RenderObjectList world;
-
-	//// ------------------------------------------------------------ generate random world ------------------------------------------------------
-	//std::shared_ptr<Material_Lambertian> ground_material = std::make_shared<Material_Lambertian>(color(0.5, 0.5, 0.5));
-	//world.add(std::make_shared<Sphere>(pos3(0, -1000, 0), 1000, ground_material));
-
-	//for (std::int32_t i = -11; i < 11; ++i)
-	//{
-	//	for (std::int32_t ii = -11; ii < 11; ++ii)
-	//	{
-	//		precision choose_mat = get_random01();
-	//		pos3 center(i + 0.9 * get_random01(), 0.2, ii + 0.9 * get_random01());
-
-	//		if ((center - pos3(4, 0.2, 0)).length() > 0.9) {
-	//			std::shared_ptr<Material> sphere_material;
-
-	//			if (choose_mat < 0.8) {
-	//				// diffuse
-	//				auto albedo = color::random() * color::random();
-	//				sphere_material = std::make_shared<Material_Lambertian>(albedo);
-	//				world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
-	//			}
-	//			else if (choose_mat < 0.95) {
-	//				// metal
-	//				auto albedo = color::random(0.5, 1);
-	//				auto fuzz = get_random(0, 0.5);
-	//				sphere_material = std::make_shared<Material_Metal>(albedo, fuzz);
-	//				world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
-	//			}
-	//			else {
-	//				// glass
-	//				sphere_material = std::make_shared<Material_Dielectric>(1.5);
-	//				world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
-	//			}
-	//		}
-	//	}
-	//}
-
-	//std::shared_ptr<Material_Dielectric> material1 = std::make_shared<Material_Dielectric>(1.5);
-	//world.add(std::make_shared<Sphere>(pos3(0, 1, 0), 1.0, material1));
-
-	//std::shared_ptr<Material_Lambertian> material2 = std::make_shared<Material_Lambertian>(color(0.4, 0.2, 0.1));
-	//world.add(std::make_shared<Sphere>(pos3(-4, 1, 0), 1.0, material2));
-
-	//std::shared_ptr<Material_Metal> material3 = std::make_shared<Material_Metal>(color(0.7, 0.6, 0.5), 0.0);
-	//world.add(std::make_shared<Sphere>(pos3(4, 1, 0), 1.0, material3));
-	//// // --------------------------------------------------------------------------------------------------------------------------------------
-
-	//// RENDER IMAGE
-	//Camera camera{ pos3 {13,2,3}, pos3{0,0,0}, 20.0, vec3{0,1,0}, 0.1, 10.0 };
-	//
-	//{
-	//	ThreadPool threadpool;
-
-	//	for (std::size_t i = 0; i < img_height; ++i)
-	//	{
-	//		//std::cout << "\rScanlines remaining: " << img_height - i - 1 << ' ' << std::flush;
-	//		for (std::size_t ii = 0; ii < img_width; ++ii)
-	//		{
-	//			threadpool.EnqueueTask([&, i, ii]
-	//				{
-	//					color pixel_color{ 0,0,0 };
-
-	//					for (std::size_t sample = 0; sample < samples_per_pixel; ++sample)
-	//					{
-	//						precision u = (ii + get_random01()) / (img_width - 1);
-	//						precision v = (i + get_random01()) / (img_height - 1);
-	//						pixel_color += ray_color(camera.get_ray(u, v), world, bounce_limit);
-	//					}
-
-	//					write_color(&img_rgb[(i * img_width + ii) * 3], pixel_color, samples_per_pixel);
-	//				});
-	//		}
-	//	}
-
-	//	threadpool.JoinAllTasks();
-	//}
-
-	//std::cout << "\ntime to write to file xo\n";
-	//stbi_flip_vertically_on_write(true);
-	//if (stbi_write_png("../../outputimage.png", img_width, img_height, 3, img_rgb, img_width * 3) == 0) std::cout << "couldn't write to PNG lolol";
-	//else std::cout << "fin.\n";
-
-	//// CLEANUP
-	//delete[] img_rgb;
 }
 
 // Helper functions
@@ -410,53 +290,66 @@ bool CreateDeviceD3D(HWND hWnd, void* img_buffer, ID3D11ShaderResourceView*& img
 		return false;
 
 	// create render target
-	//ID3D11Texture2D* pBackBuffer{ nullptr };
-	//g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
-	//if (pBackBuffer == nullptr) return false;
-	//g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
-	//pBackBuffer->Release();
+	if (!(CreateRenderTarget())) return false;
 
-	//// texture description
-	//D3D11_TEXTURE2D_DESC tex_description;
-	//ZeroMemory(&tex_description, sizeof(tex_description));
-	//tex_description.Width = static_cast<UINT>(img_width);
-	//tex_description.Height = static_cast<UINT>(img_height);
-	//tex_description.MipLevels = 1;
-	//tex_description.ArraySize = 1;
-	//tex_description.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	//tex_description.SampleDesc.Count = 1;
-	//tex_description.Usage = D3D11_USAGE_DYNAMIC;
-	//tex_description.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-	//tex_description.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	// texture description
+	D3D11_TEXTURE2D_DESC tex_description;
+	ZeroMemory(&tex_description, sizeof(tex_description));
+	tex_description.Width = static_cast<UINT>(img_width);
+	tex_description.Height = static_cast<UINT>(img_height);
+	tex_description.MipLevels = 1;
+	tex_description.ArraySize = 1;
+	tex_description.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	tex_description.SampleDesc.Count = 1;
+	tex_description.Usage = D3D11_USAGE_DYNAMIC;
+	tex_description.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	tex_description.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 
-	//// bind image buffer to texture sub-resource
-	//D3D11_SUBRESOURCE_DATA tex_resource;
-	//tex_resource.pSysMem = img_buffer;
-	//tex_resource.SysMemPitch = tex_description.Width * 4;
-	//tex_resource.SysMemSlicePitch = 0;
+	// bind image buffer to texture sub-resource
+	D3D11_SUBRESOURCE_DATA tex_resource;
+	tex_resource.pSysMem = img_buffer;
+	tex_resource.SysMemPitch = tex_description.Width * 4;
+	tex_resource.SysMemSlicePitch = 0;
 
-	//// texture view description
-	//D3D11_SHADER_RESOURCE_VIEW_DESC srv_desc;
-	//ZeroMemory(&srv_desc, sizeof(srv_desc));
-	//srv_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	//srv_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	//srv_desc.Texture2D.MipLevels = 1;
-	//srv_desc.Texture2D.MostDetailedMip = 0;
+	// texture view description
+	D3D11_SHADER_RESOURCE_VIEW_DESC srv_desc;
+	ZeroMemory(&srv_desc, sizeof(srv_desc));
+	srv_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	srv_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	srv_desc.Texture2D.MipLevels = 1;
+	srv_desc.Texture2D.MostDetailedMip = 0;
 
-	//// create the texture view
-	//ID3D11Texture2D* texture{ nullptr };
-	//g_pd3dDevice->CreateTexture2D(&tex_description, &tex_resource, &texture);
-	//if (texture == nullptr) return false;
-	//g_pd3dDevice->CreateShaderResourceView(texture, &srv_desc, &img_buffer_srv);
-	//texture->Release();
+	// create the texture view
+	ID3D11Texture2D* texture{ nullptr };
+	g_pd3dDevice->CreateTexture2D(&tex_description, &tex_resource, &texture);
+	if (texture == nullptr) return false;
+	g_pd3dDevice->CreateShaderResourceView(texture, &srv_desc, &img_buffer_srv);
+	texture->Release();
 
 	return true;
 }
 
+bool CreateRenderTarget()
+{
+	ID3D11Texture2D* pBackBuffer{ nullptr };
+	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+	if (pBackBuffer == nullptr) return false;
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+	pBackBuffer->Release();
+
+	return true;
+}
+
+void CleanupRenderTarget()
+{
+	if (g_mainRenderTargetView != nullptr) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+}
+
+
 void CleanupDeviceD3D(ID3D11ShaderResourceView*& img_buffer_srv)
 {
 	if (img_buffer_srv != nullptr) { img_buffer_srv->Release(); img_buffer_srv = nullptr; }
-	if (g_mainRenderTargetView != nullptr) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+	CleanupRenderTarget();
 	if (g_pSwapChain != nullptr) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
 	if (g_pd3dDeviceContext != nullptr) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
 	if (g_pd3dDevice != nullptr) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
@@ -480,9 +373,9 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_SIZE:
 		if (g_pd3dDevice != nullptr && wParam != SIZE_MINIMIZED)
 		{
-			//CleanupRenderTarget();
-			//g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
-			//CreateRenderTarget();
+			CleanupRenderTarget();
+			g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+			CreateRenderTarget();
 		}
 		return 0;
 	case WM_SYSCOMMAND:
